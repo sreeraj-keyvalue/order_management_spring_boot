8022dd31631854f45251d4304f2629c9a3473ca0
Sreeraj Rajeendran <sreeraj@keyvalue.systems>
2023-04-20 10:45:09 +0530

    Fix: Add missing fields in product entity

--- src/main/java/com/ecommerce/order/model/Product.java
+++ src/main/java/com/ecommerce/order/model/Product.java
@@ -4,11 +4,13 @@ import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @Entity
 public class Product extends BaseEntity {
 
@@ -18,4 +20,10 @@ public class Product extends BaseEntity {
     @ManyToOne
     @JoinColumn
     private Category category;
+
+    private String image;
+
+    private String description;
+
+    private double unitPrice;
 }
